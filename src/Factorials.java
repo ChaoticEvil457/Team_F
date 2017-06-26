2a59e2a212509c4243528cdbf6d478278775e4df
SummerTech <SummerTech@10.144.4.26>
2017-06-26 13:33:57 -0400

    hfv

---
+++ src/Factorials.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorials {
+	
+	static Scanner scan=new Scanner(System.in);
+	static int product=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int f=scan.nextInt();
+		System.out.print(f);
+		System.out.print("! is ");
+		System.out.println(product);
+	}
+	public static void Factorials(){
+		
+		
+		Factorials();
+		
+		
+	}
+}
