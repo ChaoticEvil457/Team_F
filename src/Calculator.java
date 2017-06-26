79715dee10d8e09a1f0a666e2b5e5808a7b3d52e
SummerTech <SummerTech@10.144.4.26>
2017-06-26 11:12:42 -0400

    dsaf

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,1 +1,120 @@
-import java.util.Scanner;public class Calculator {static int numberOne = 0;static int numberTwo = 0;static String symbol = "";static boolean valid = false;static boolean validNumber = false;static Scanner scan = new Scanner(System.in);public static void main(String[] args) {while(valid==false){if(symbol.equals("*")){System.out.println(numberOne*numberTwo);valid = true;}else if(symbol.equals("-")){System.out.println(numberOne-numberTwo);valid = true;}else if(symbol.equals("/")){System.out.println(numberOne/numberTwo);valid = true;}else if(symbol.equals("+")){System.out.println(numberOne+numberTwo);valid = true;}else if(symbol!="+"||symbol!="-"||symbol!="/"||symbol!="*"){System.out.print("Sorry ");System.out.print(symbol);System.out.println(" is not a valid symbol. Please re-enter your symbol");System.out.println("Input the symbol that you would like to use");symbol=scan.next();}}}public static void numberInput(){System.out.println("Put in your first number.");while(validNumber==true){try{numberOne=scan.nextInt();validNumber=true;}catch(Exception e){System.out.print(numberOne);System.out.println(" is not a number.");validNumber=true;}}System.out.println("Input the symbol that you would like to use");symbol=scan.next();System.out.println("Put in your second number.");numberTwo=scan.nextInt();}}
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	static int numberOne = 0;
+	
+	static int numberTwo = 0;
+	
+	static String symbol = "";
+	
+	static boolean valid = false;
+	
+	static boolean validNumber = true;
+	
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+		numberInput();
+
+			if(symbol.equals("*")){
+
+				System.out.println(numberOne*numberTwo);
+
+				valid = true;
+
+			}
+
+			else if(symbol.equals("-")){
+
+				System.out.println(numberOne-numberTwo);
+
+				valid = true;
+
+			}
+
+			else if(symbol.equals("/")){
+
+				System.out.println(numberOne/numberTwo);
+
+				valid = true;}
+
+			else if(symbol.equals("+")){
+
+				System.out.println(numberOne+numberTwo);
+
+				valid = true;
+
+			}
+	}
+	public static void numberInput(){
+
+
+
+		while(validNumber==true){
+
+			try{
+				System.out.println("Put in your first number.");
+
+				numberOne=new Scanner(System.in).nextInt();
+
+				validNumber=false;
+
+			}
+
+			catch(InputMismatchException e){
+
+				System.out.println("That is not a number.");
+
+				validNumber=true;
+
+			}
+
+		}
+
+		System.out.println("Input the symbol that you would like to use");
+
+		symbol=scan.next();
+		while(valid==false){
+		if(symbol!="+"||symbol!="-"||symbol!="/"||symbol!="*"){
+
+			System.out.print("Sorry ");
+
+			System.out.print(symbol);
+
+			System.out.println(" is not a valid symbol. Please re-enter your symbol");
+
+			System.out.println("Input the symbol that you would like to use");
+
+			symbol=scan.next();
+
+		}
+		}
+
+		while(validNumber==false){
+
+			System.out.println("Put in your second number.");
+
+			try{
+
+				numberTwo=new Scanner(System.in).nextInt();
+
+				validNumber=true;
+
+			}
+
+			catch(InputMismatchException e){
+
+				System.out.print(numberTwo);
+				
+				System.out.println(" is not a number.");
+				
+				validNumber=false;
+				
+			}
+		}
+	}
+}
