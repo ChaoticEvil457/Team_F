ff589774bfebc5952e9e1f60d25b60f48f511c3f
SummerTech <SummerTech@10.144.4.26>
2017-06-26 09:11:33 -0400

    yo dawg I heard you liked code so I commit and pushed

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+		int numberOne
+		int numberTwo
+		String 
+		
+		
+	}
+
+}
