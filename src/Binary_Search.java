b4c6e8b170e1c6e3619656b86c0b6b1d7d284eb0
SummerTech <SummerTech@10.144.4.26>
2017-06-29 11:15:08 -0400

    erawa

--- src/Binary_Search.java
+++ src/Binary_Search.java
@@ -5,10 +5,11 @@ public class Binary_Search {
 	static Scanner scan=new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int ray[]=new int[100];
-
+		
+		System.out.println("Input a number from 1 to 98");
+		
 		int number=scan.nextInt();
 
 		int z=0;
@@ -25,30 +26,69 @@ public class Binary_Search {
 		
 	}
 	public static int finder(int number,int ray[]){
-
-		if(ray[ray.length/2-1]==number){
+		try{
+		if(ray[ray.length/2]==number){
 			System.out.print("The number you are looking for is ");
 			System.out.println(ray[ray.length/2]);
 		}
-		else if(ray[ray.length/2-1]>number){
-			int ray2[]=new int[ray.length/2-1];
-			int z=ray.length/2-1;
-			while(ray2[ray.length/2-2]==0){
-				ray2[z]=ray[z];
-				z++;
-			}
-			finder(number,ray2);
-		}
-		else if(ray[ray.length/2-1]<number){
-			int ray2[]=new int[ray.length/2-1];
-			int z=0;
-			while(ray2[ray.length/2-1]==0){
-				ray2[z]=ray[z];
-				z++;
-			}
-			finder(number,ray2);
-		}
 		
+		else if(ray[ray.length/2]<number){
+			int ray2[]=new int[ray.length/2+2];
+			int z=ray.length/2-1;
+			int z2=0;
+			while(ray2[ray.length/2+1]==0){
+
+				ray2[z2]=ray[z];
+				z++;
+				z2++;
+
+			}
+			finder(number,ray2);
+			
+		}
+		else if(ray[ray.length/2]>number){
+			int ray2[]=new int[ray.length/2+1];
+			int z=0;
+			while(ray2[ray.length/2]==0){
+
+				ray2[z]=ray[z];
+				z++;
+			}
+			finder(number,ray2);
+		}
+		}
+		catch(Exception e){
+			if(ray[ray.length/2]==number){
+				System.out.print("The number you are looking for is ");
+				System.out.println(ray[ray.length/2]);
+			}
+			
+			else if(ray[ray.length/2-1]<number){
+				int ray2[]=new int[ray.length/2];
+				int z=ray.length/2-1;
+				int z2=0;
+				while(ray2[ray.length/2-1]==0){
+
+					ray2[z2]=ray[z];
+					z++;
+					z2++;
+
+				}
+				finder(number,ray2);
+				
+			}
+			else if(ray[ray.length/2]>number){
+				int ray2[]=new int[ray.length/2];
+				int z=0;
+				while(ray2[ray.length/2]==0){
+
+					ray2[z]=ray[z];
+					z++;
+				}
+				finder(number,ray2);
+			}
+		}
 		return(1);
 	}
+	//49 b/c 6, 78+ b/c 52
 }
