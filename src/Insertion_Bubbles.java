98ad67a34e868758e0af3b15ba9efc5f71efd4c7
SummerTech <SummerTech@10.144.4.26>
2017-06-30 10:00:53 -0400

    xzcv

---
+++ src/Insertion_Bubbles.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Insertion_Bubbles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer[] m={9,3,5,2,4,1};
+
+		ArrayList<Integer> ray=new ArrayList<Integer>();
+
+		ray.addAll(Arrays.asList(m));
+
+		int x=0;
+		int y=1;
+		int j=0;
+
+		while(j<ray.size()){
+
+
+			if(ray.get(x)>ray.get(y)){
+
+
+
+			}
+			
+			else{
+				j++;
+				
+			}
+		}
+	}
+}
