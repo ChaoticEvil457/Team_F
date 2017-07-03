faecc5b09c00a3f7abf66e1b3615f8419e4a919a
SummerTech <SummerTech@10.144.4.26>
2017-07-03 12:37:00 -0400

    fsdgfdsg

--- src/BogoBogoSort.java
+++ src/BogoBogoSort.java
@@ -1,37 +1,43 @@
-
-public class BogoSort {
+public class BogoBogoSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] ray={4,1,5,2,3,76,346,253};
+		int[] ray={4,1,5,2,3,6,7,8,9,0,10,11,12,13,14,15};
 
 		int z=0;
 		int g=0;
 		int f=0;
 		int l=0;
 		int counter=-1;
-		
+
 		while(counter<ray.length){
-			
+
+
 			int x=(int) (Math.random()*ray.length);
-			
+
 			int y=(int) (Math.random()*ray.length);
 			if(z+1>=ray.length){
 				z=0;
 			}
 
 			if(ray[z]>ray[z+1]){
-				
-				f=ray[x];
-				g=ray[y];
-				
-				ray[x]=g;
-				ray[y]=f;
-				
-				counter=-1;
-				z++;
-				
+				int k=0;
+				while(k<ray.length){
+					
+					int p=(int) (Math.random()*ray.length);
+					int o=ray[k];
+					int	i=ray[p];
+					
+					ray[k]=i;
+					ray[p]=o;
+					
+					
+					k++;
+				}
+			
+			counter=-1;
+			z++;
 			}
 			else{
 				z++;
@@ -40,19 +46,20 @@ public class BogoSort {
 			if(z+1>=ray.length){
 				z=0;
 			}
-			
+
 			l++;
 			System.out.println(l);
-			
+
 		}
+		
 		l=0;
 		while(l<ray.length){
-			
-		System.out.print(ray[l]);
-		System.out.print(",");
-		
-		l++;
-		
+
+			System.out.print(ray[l]);
+			System.out.print(",");
+
+			l++;
+
 		}
 
 	}
