bbb2c2d2da95a0ca21c2d572b3fae4d89e024847
SummerTech <SummerTech@10.144.4.17>
2017-07-11 12:44:50 -0400

    gbv

--- src/Hash_Browns.java
+++ src/Hash_Browns.java
@@ -25,7 +25,7 @@ public class Hash_Browns {
 		h.put("Agamemnon",0.65);
 		while(true){
 
-			System.out.println("Please enter a valid command (print, add, or add account)");
+			System.out.println("Please enter a valid command (print, add, withdraw, or add account)");
 
 			String input=scan.next();
 
@@ -51,6 +51,33 @@ public class Hash_Browns {
 								add(x,y);
 								
 								hi1=true;
+								
+							}
+							catch(Exception k){
+
+								System.out.println("That is not a valid account name or amount of money");
+
+							}
+						}
+				System.out.println(h);
+			}
+			else if(input.equalsIgnoreCase("withdraw")){
+				boolean hi12=false;
+
+						while(hi12==false){
+							try{
+
+								System.out.println("Enter the name of the account you would like to remove money from");
+
+								String x=scan.next();
+
+								System.out.println("Enter the amount of money you would like to withdraw");
+
+								double y=scan.nextDouble();
+
+								sub(x,y);
+								
+								hi12=true;
 								
 							}
 							catch(Exception k){
@@ -95,5 +122,18 @@ public class Hash_Browns {
 		z+=y;
 
 		h.put(x,z);
+	}
+	public static void sub(String x,Double y){
+
+		double z=h.get(x);
+
+		z-=y;
+
+		h.put(x,z);
+	}
+	public static void printer(){
+		
+		
+		
 	}
 }
