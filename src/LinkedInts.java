928694cb7135b77d4654a0139b5b97b5b1f870af
SummerTech <SummerTech@10.144.4.17>
2017-07-13 13:30:48 -0400

    sdfg

--- src/LinkedInts.java
+++ src/LinkedInts.java
@@ -3,166 +3,198 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class LinkedInts implements List{
+public class LinkedInts{
 
-	 Nodes nodes;
-	 
-	 Nodes a;
-	 
-	 int length;
-	
+	Nodes nodes;
+
+
+
+	int length;
+
 	public LinkedInts(){
-		
+
 		nodes=null;
 
 		length=0;
-		
+
 	}
-	
-	
-	@Override
+
+
 	public boolean add(int hello) {
 		// TODO Auto-generated method stub
-		
+		Nodes a=new Nodes(hello,null);
+
+		Nodes b=nodes;
+
 		if(nodes==null){
-			
-			a=hello;
-			
+
+			nodes=a;
+
 		}
-		
+
+		while(b.Public()!=null){
+
+			b=b.Public();
+
+		}
+		b.publicstaticint(a);
+
 		return true;
 	}
 
-	@Override
+
 	public void add(int index, Object element) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
+
 	public boolean addAll(Collection c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+
 	public boolean addAll(int index, Collection c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
+
 	public boolean contains(Object o) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+
 	public boolean containsAll(Collection c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public Object get(int index) {
+
+	public int get(int c) {
 		// TODO Auto-generated method stub
-		return null;
+
+		Nodes e=nodes;
+		
+		int f=0;
+
+		while(f<c){
+
+			e=e.Public();
+
+			f++;
+		}
+		
+		return(e.Static());
+		
 	}
 
-	@Override
+
 	public int indexOf(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+
 	public Iterator iterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public int lastIndexOf(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+
 	public ListIterator listIterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public ListIterator listIterator(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public boolean remove(Object o) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+
 	public Object remove(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public boolean removeAll(Collection c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+
 	public boolean retainAll(Collection c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public Object set(int index, Object element) {
+
+	public Object set(int c, Object g) {
 		// TODO Auto-generated method stub
-		return null;
+		Nodes e=nodes;
+		h3hkl'lhk;
+		int f=0;
+
+		while(f<c){
+
+			e=e.Public();
+
+			f++;
+		}
+		
+		return(e.Static());
 	}
 
-	@Override
+
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+
 	public List subList(int fromIndex, int toIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+
 	public Object[] toArray(Object[] a) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
